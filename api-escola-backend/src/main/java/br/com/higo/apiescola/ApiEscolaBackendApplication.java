c948e202715a232db6ee909cf80245aa4cc982c3
Higo <higoedu@gmail.com>
2026-09-18 17:28:48 -0300

    chore: inicializa projeto Spring Boot

---
+++ api-escola-backend/src/main/java/br/com/higo/apiescola/ApiEscolaBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.higo.apiescola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEscolaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiEscolaBackendApplication.class, args);
+    }
+
+}
---
+++ api-escola-backend/src/test/java/br/com/higo/apiescola/ApiEscolaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.higo.apiescola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEscolaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
